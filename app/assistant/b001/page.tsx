579ff5a2ae21c2a9630b385d1b3ae51fdfd99a21
David Guo <davidguo123456@gmail.com>
2025-06-05 16:30:10 -0400

    bug fix

--- app/assistant/b001/page.tsx
+++ app/assistant/b001/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import SeekerSessionPage from '@/components/SeekerSessionPage/SeekerSessionPage';
+import AssistantSessionPage from '@/components/AssistantSessionPage/AssistantSessionPage';
 import { B_PATHS } from '@/app/constants';
 import { shuffleArray } from '@/lib/utils/random';
 import { usePathname } from 'next/navigation';
 
 export default function Page() {
   const sessionCode = usePathname().split('/').at(-1) ?? '';
-  return <SeekerSessionPage sessionCode={sessionCode} productJsonPaths={shuffleArray(B_PATHS, sessionCode)} />;
+  return <AssistantSessionPage sessionCode={sessionCode} productJsonPaths={shuffleArray(B_PATHS, sessionCode)} />;
 }
