19e08ee1c147d65aeabf0023f5deb3ff7bbd0a89
David Guo <davidguo123456@gmail.com>
2025-06-04 21:00:46 -0400

    fix paths

--- app/assistant/a000/page.tsx
+++ app/assistant/a000/page.tsx
@@ -6,16 +6,17 @@ const productJsonPaths = [
   'https://davidguo123456.github.io/d3m-2025-summer/outer_1.json',
   'https://davidguo123456.github.io/d3m-2025-summer/outer_2.json',
   'https://davidguo123456.github.io/d3m-2025-summer/outer_3.json',
-  'https://davidguo123456.github.io/d3m-2025-summer/outer_1.json',
-  'https://davidguo123456.github.io/d3m-2025-summer/outer_2.json',
-  'https://davidguo123456.github.io/d3m-2025-summer/outer_3.json',
-  'https://davidguo123456.github.io/d3m-2025-summer/outer_1.json',
-  'https://davidguo123456.github.io/d3m-2025-summer/outer_2.json',
-  'https://davidguo123456.github.io/d3m-2025-summer/outer_3.json',
-  'https://davidguo123456.github.io/d3m-2025-summer/outer_1.json',
-  'https://davidguo123456.github.io/d3m-2025-summer/outer_2.json',
-  'https://davidguo123456.github.io/d3m-2025-summer/outer_3.json',
+  'https://davidguo123456.github.io/d3m-2025-summer/outer_4.json',
+  'https://davidguo123456.github.io/d3m-2025-summer/outer_5.json',
+  'https://davidguo123456.github.io/d3m-2025-summer/outer_6.json',
+  'https://davidguo123456.github.io/d3m-2025-summer/outer_7.json',
+  'https://davidguo123456.github.io/d3m-2025-summer/outer_8.json',
+  'https://davidguo123456.github.io/d3m-2025-summer/outer_9.json',
+  'https://davidguo123456.github.io/d3m-2025-summer/outer_10.json',
+  'https://davidguo123456.github.io/d3m-2025-summer/outer_11.json',
+  'https://davidguo123456.github.io/d3m-2025-summer/outer_12.json',
 ];
+
 export default function Page() {
   return <AssistantSessionPage sessionCode="a000" productJsonPaths={productJsonPaths} />;
 }
