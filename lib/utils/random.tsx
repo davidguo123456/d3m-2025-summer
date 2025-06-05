4fbf1db2a48db2f3bf04130177d2b241fac994f6
David Guo <davidguo123456@gmail.com>
2025-06-05 12:36:42 -0400

    linting

--- lib/utils/random.tsx
+++ lib/utils/random.tsx
@@ -15,7 +15,7 @@ function seededRandomGenerator(seed: string): () => number {
     a |= 0;
     a = a + 0x6D2B79F5 | 0;
     let t = Math.imul(a ^ (a >>> 15), 1 | a);
-    t = (t + Math.imul(t ^ (t >>> 7), 61 | t)) ^ t;
+    t ^= (t + Math.imul(t ^ (t >>> 7), 61 | t));
     return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
   };
 }
